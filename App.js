786384698e936d35c5ea8ee12f1bb89694e621b2
Mikael Melo <mikaelsilva1940@hotmail.com>
2023-03-24 08:53:09 -0300

    Criado WebView com o respectivo Site e também adicionado useState que indica carregamento de pagina

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from "react-native-webview"
+import { ActivityIndicator } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isLoading && <ActivityIndicator />}
+      <WebView source={{ uri: 'https://13gac.eb.mil.br/' }}
+        onLoad={() => setIsLoading(false)}
+      />
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  ActivityIndicator: {
+    flex: 1,
+    top: 200,
+    justifyContent: 'center',
+    alignContent: 'center',
+    height: 200,
+    width: 200,
+  }
 });
